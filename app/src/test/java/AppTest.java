f24c6528245b89196fadd8141aee5440e818cba9
andreevmo <maxiandreev1994@gmail.com>
2022-10-29 15:35:36 +0400

    feat: add tests

---
+++ app/src/test/java/AppTest.java
@@ -0,0 +1,33 @@
+import hexlet.code.App;
+import io.javalin.Javalin;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+    private static Javalin app;
+    private static String baseUrl;
+
+    @BeforeAll static void beforeAll() {
+        app = App.getApp();
+        app.start();
+        baseUrl = "http://localhost:" + app.port();
+    }
+
+    @AfterAll static void afterAll() {
+        app.stop();
+    }
+
+    @Test void testApp() {
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/")
+                .asString();
+        String content  = response.getBody();
+
+        assertThat(content).isEqualTo("Hello world!");
+    }
+}
